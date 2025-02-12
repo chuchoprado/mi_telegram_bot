8abe739ddb74adda403e7c8f9b67ce2ddeaa89a7
chuchoprado <43660933+chuchoprado@users.noreply.github.com>
2025-02-12 10:27:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -51,6 +51,10 @@ class CoachBot:
         """Maneja el comando /start"""
         logger.info(f"✅ Comando /start recibido de {update.message.chat.id}")
 
+        await update.message.reply_text(
+            "¡Hola! Bienvenido al Coach Meditahub, por favor proporciona tu email para acceder a tu asistente."
+        )
+
         if not await self.is_user_whitelisted(update.message.chat.id):
             await update.message.reply_text(
                 "Lo siento, tu correo no está en la lista blanca. No puedes acceder al bot."
@@ -58,7 +62,7 @@ class CoachBot:
             return
 
         await update.message.reply_text(
-            "¡Hola! Bienvenido al Coach Meditahub, por favor proporciona tu email para acceder a tu asistente."
+            "¡Hola! Soy El Coach Bot. ¿En qué puedo ayudarte hoy?"
         )
 
     def _init_sheets(self):
