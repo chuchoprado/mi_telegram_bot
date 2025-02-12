6cecd6975703090857e674ba67fcabd468160185
chuchoprado <43660933+chuchoprado@users.noreply.github.com>
2025-02-12 15:39:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import logging
 
 # Configurar logging
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levellevelname)s - %(message)s',
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 logger = logging.getLogger(__name__)
