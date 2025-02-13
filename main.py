728e264254219aa1d296b00fd19464175a8b9ab2
chuchoprado <43660933+chuchoprado@users.noreply.github.com>
2025-02-13 12:25:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import os
 import asyncio
 import io
 from fastapi import FastAPI, Request
-from telegram import Update, ChatAction  # Import ChatAction here
+from telegram import Update
+from telegram.constants import ChatAction  # Import ChatAction from telegram.constants
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
